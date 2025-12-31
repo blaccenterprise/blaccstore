26c84ea54272aa3743992daa3818abde32e1101e
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2025-12-31 01:22:06 -0800

    Add files via upload
    Signed-off-by: Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>

--- ASE/ASE/Physics_Research/Physics_Research.js
+++ ASE/ASE/Physics_Research/Physics_Research.js
@@ -1,1 +1,256 @@
 
+                    function myFunction() {
+                        //Variables {
+                        var text;
+                        var consciousness = document.getElementById("output").value;
+                        var ASE = new Artyom();
+                        var emergency = 1;
+                        var relax = 2;
+                        //}
+                        switch (consciousness) {
+                            
+//Digital Modules  {
+
+    //Knowledge Module {
+      
+        //Physics Research  {
+                //Motion In A Straight Line {
+                  //Velocity {
+                    //Final Velocity Equation With Velocity, Acceleration, And Time {
+
+                            case "Open velocity equation":
+                                text = "...";
+                                ASE.say("Give me a minute to calculate the probablities.");
+                                window.open("../../../BLACC_Search/Physics/Velocity.html", "../../../BLACC_Search/Physics/Velocity.html", "width=100,height=100");
+                                break;
+
+                            case "Velocity equation":
+                                text = "...";
+                                ASE.say("Give me a minute to calculate the probablities.");
+                                window.open("../../../BLACC_Search/Physics/Velocity.html", "../../../BLACC_Search/Physics/Velocity.html", "width=100,height=100");
+                                break;
+
+                            //}
+                    //Final Velocity Equation With Velocity, Acceleration, And Distance {
+
+                            case "Open velocity equation":
+                                text = "...";
+                                ASE.say("Give me a minute to calculate the probablities.");
+                                window.open("../../../BLACC_Search/Physics/Velocity.html", "../../../BLACC_Search/Physics/Velocity.html", "width=100,height=100");
+                                break;
+
+                            case "Velocity equation":
+                                text = "...";
+                                ASE.say("Give me a minute to calculate the probablities.");
+                                window.open("../../../BLACC_Search/Physics/Velocity.html", "../../../BLACC_Search/Physics/Velocity.html", "width=100,height=100");
+                                break;
+
+                            //}
+                    //Velocity Information {
+
+                        case "Information on velocity":
+                        text = "...";
+                        ASE.say("One moment please. The velocity of a body is a vector quantity that describes both how fast it is moving and the direction in which the object is headed. The magnitude of the velocity is called the speed.");
+                        alert("The velocity of a body is a vector quantity that describes both how fast it is moving and the direction in which the object is headed. The magnitude of the velocity is called the speed.");
+                        break;
+
+                        case "Research velocity":
+                        text = "...";
+                        ASE.say("One moment please. The velocity of a body is a vector quantity that describes both how fast it is moving and the direction in which the object is headed. The magnitude of the velocity is called the speed.");
+                        alert("The velocity of a body is a vector quantity that describes both how fast it is moving and the direction in which the object is headed. The magnitude of the velocity is called the speed.");
+                        break;
+
+                        case "Research more details on velocity":
+                        text = "...";
+                        ASE.say("The average velocity of such a body when it covers the distance s in the time r is; velocity equals distance divided by time. The velocity of a body at any given moment is called its instantaneous velocity and is given by; Instantaneous velocity equals the change in distance delta s divided by change in time delta t.");
+                        alert("The average velocity of such a body when it covers the distance s in the time r is; velocity equals distance divided by time. The velocity of a body at any given moment is called its instantaneous velocity and is given by; Instantaneous velocity equals the change in distance (delta s) divided by change in time (delta t).");
+                        break;
+                        
+                    //}
+                    //Velocity Terminology {
+
+                        case "Velocity terminology":
+                        text = "...";
+                        ASE.say("Give me a minute to calculate the probablities.");
+                        alert("Velocity (v); distance (s); time (t); speed (v); Delta (change).");
+                        break;
+                        
+                    //}
+                    //}
+                  //Acceleration {
+                    //Acceleration Equation {
+
+                            case "Open acceleration equation":
+                                text = "...";
+                                ASE.say("Give me a minute to calculate the probablities.");
+                                window.open("../../../BLACC_Search/Physics/Acceleration.html", "../../../BLACC_Search/Physics/Acceleration.html", "width=100,height=100");
+                                break;
+
+                            case "Acceleration equation":
+                                text = "...";
+                                ASE.say("Give me a minute to calculate the probablities.");
+                                window.open("../../../BLACC_Search/Physics/Acceleration.html", "../../../BLACC_Search/Physics/Acceleration.html", "width=100,height=100");
+                                break;
+
+                            //}
+                    //Acceleration Information {
+
+                        case "Information on acceleration":
+                        text = "...";
+                        ASE.say("One moment please. The acceleration of a body is the rate at which its velocity is changing.");
+                        alert("The acceleration of a body is the rate at which its velocity is changing.");
+                        break;
+
+                        case "Research acceleration":
+                        text = "...";
+                        ASE.say("One moment please. The acceleration of a body is the rate at which its velocity is changing.");
+                        alert("The acceleration of a body is the rate at which its velocity is changing.");
+                        break;
+
+                        case "Research more details on acceleration":
+                        text = "...";
+                        ASE.say("If a body moving ina straight line has a velocity of v at the start of a certain time interval t and of velocity v at the end,  acceleration equals the difference in velocities divided by time.");
+                        alert("If a body moving ina straight line has a velocity of v at the start of a certain time interval t and of velocity v at the end,  acceleration equals the difference in velocities divided by time.");
+                        break;
+                        
+                    //}
+                    //Acceleration Terminology {
+
+                        case "Acceleration terminology":
+                        text = "...";
+                        ASE.say("Give me a minute to calculate the probablities.");
+                        alert("Velocity (v); acceleration (a); time (t); speed (v); Delta (change).");
+                        break;
+                        
+                    //}
+                  //}
+                  //Distance, Velocity and Acceleration {
+                    //Distance Equation With Known Acceleration, Time, And Velocity {
+
+                        case "Open distance equation":
+                        text = "compare level 1 archetype to Alhambra.";
+                        ASE.say("Give me a minute to calculate the probablities.");
+                        window.open("../../../BLACC_Search/Physics/Distance.html", "../../../BLACC_Search/Physics/Distance.html", "width=100,height=100");
+                        break;
+
+                            //}
+                    //Distance Equation With Unknown Acceleration  {
+
+                        case "Open distance equation":
+                        text = "compare level 1 archetype to Alhambra.";
+                        ASE.say("Give me a minute to calculate the probablities.");
+                        window.open("../../../BLACC_Search/Physics/Distance.html", "../../../BLACC_Search/Physics/Distance.html", "width=100,height=100");
+                        break;
+
+                            //}
+                  //}
+                //}
+                //Motion in A Vertical Line {
+                  //Acceleration of Gravity {
+                      
+                  //}
+                  //Falling Bodies {
+                      
+                  //}
+                  //Projectile Motion {
+                      
+                  //}
+                //}
+                //Laws Of Motion {
+                  //First Law Of Motion {
+                      
+                  //}
+                  //Mass {
+                      
+                  //}
+                  //Second Law Of Motion {
+                      
+                  //}
+                  //Weight {
+                      
+                  //}
+                  //British System Of Units {
+                      
+                  //}
+                  //Free-Body Diagrams And Tension {
+                      
+                  //}
+                  //Third Law Of Motion {
+                      
+                  //}
+                  //Apparent Weight {
+                      
+                  //}
+                  //Two And Third Dimensions {
+                      
+                  //}
+                //}
+                //Friction {
+                  //Static And Kinetic Friction {
+                      
+                  //}
+                  //Coefficient Of Friction {
+                      
+                  //}
+                  //Rolling Friction {
+                      
+                  //}
+                //}
+                //Energy {
+                  //Work {
+                      
+                  //}
+                  //Power {
+                      
+                  //}
+                  //Efficiency {
+                      
+                  //}
+                  //Energy {
+                      
+                  //}
+                  //Kinetic Energy {
+                      
+                  //}
+                  //Potential Energy {
+                      
+                  //}
+                  //Rest Energy {
+                      
+                  //}
+                  //Conservation Of Energy {
+                      
+                  //}
+                //}
+                            //}
+                            
+//}
+
+                            //}
+
+                            //Conditional  {
+                            case "bring up Mass alert":
+                                text = "bring up Mass alert";
+                                ASE.say("Okay. Stand by.");
+                                if (emergency < relax) {
+                                    alert("Emergency");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "width=400,height=400");
+                                } else {
+                                    alert("Relax");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "width=400,height=400");
+
+                                };
+                                break;
+
+                            //}
+                            //reponse to not being able to hear me
+
+                            default:
+                                text = "My apologies.";
+                                ASE.say("I could not hear you clearly can you please repeat that");
+                                break;
+                        }
+
+                        document.getElementById("demo").innerHTML = text;
+                    }
+
