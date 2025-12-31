cab18c6f29b4590c04a826fefe5a0a3aed9258c2
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2025-12-31 00:56:04 -0800

    Add files via upload
    Signed-off-by: Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>

--- ASE/ASE/Mathematical_Operations/Mathematical_Operations.js
+++ ASE/ASE/Mathematical_Operations/Mathematical_Operations.js
@@ -1,1 +1,416 @@
 
+                    function myFunction() {
+                                              //Variables {
+                        var text;
+                        var consciousness = document.getElementById("output").value;
+                        var ASE = new Artyom();
+                        var emergency = 1;
+                        var relax = 2;
+                        //}
+
+                        switch (consciousness) {
+                            
+//Digital Modules  {
+
+    //Knowledge Module {
+    
+        //MATHEMATICAL OPERATIONS  {
+                //Statistics  {
+                    //Descriptive Statistics  {
+                        //Class Width  {
+    case "what is the equation for class width":
+    text = "class width equals highest value minus lowest value divided by number of classes.";
+    ASE.say("class width equals highest value minus lowest value divided by number of classes.");
+    break;
+                    
+    case "open class width":
+    text = "(Highest Value - Lowest Value) / Number Of Classes.";
+    window.open("../../../BLACC_Search/Math_Operations/Class_Width.html", "../../../BLACC_Search/Math_Operations/Class_Width.html", "width=400,height=400");
+    break;
+//}
+                        //Class Midpoint  {
+    case "what is the equation for class midpoint":
+    text = "class midpoint equals upper limit minus lower limit divided by 2.";
+    ASE.say("class midpoint equals upper limit minus lower limit divided by 2.");
+    break;
+    
+    case "open class midpoint":
+    text = "upper limit - lower limit / 2.";
+    window.open("../../../BLACC_Search/Math_Operations/Midpoint.html", "../../../BLACC_Search/Math_Operations/Midpoint.html", "width=400,height=400");
+    break;
+                      //}
+                        //Terminology  {
+                    //n  {
+    case "what does the lowercase n variable represent":
+    text = "The lowercase n represents the variable for identifying the size of sample population.";
+    ASE.say("The lowercase n represents the variable for identifying the size of sample population.");
+    break;
+
+    case "what does the lowercase in variable represent":
+    text = "The lowercase n represents the variable for identifying the size of sample population.";
+    ASE.say("The lowercase n represents the variable for identifying the size of sample population.");
+    break;
+                 //}
+                    //N  {
+    case "what does the capital in variable represent":
+    text = "The capital N represents the variable for identifying the size of population.";
+    ASE.say("The capital N represents the variable for identifying the size of population.");
+    break;
+
+    case "what does the capital N variable represent":
+    text = "The capital N represents the variable for identifying the size of population.";
+    ASE.say("The capital N represents the variable for identifying the size of population.");
+    break;
+                      
+    case "what does the capital n variable represent":
+    text = "The capital N represents the variable for identifying the size of population.";
+    ASE.say("The capital N represents the variable for identifying the size of population.");
+    break;
+                      //}
+                    //frequency variable  f  {
+    case "what does the frequency variable represent":
+    text = "The fequency variable represents the number of times as value occurs within a specific class.";
+    ASE.say("The fequency variable represents the number of times as value occurs within a specific class.");
+    break;
+//}
+                    //Summation  {
+    case "what does the summation variable represent":
+    text = "The summation variable represents the sum of all values from the operation.";
+    ASE.say("The summation variable represents the sum of all values from the operation.");
+    break;
+
+    case "what is summation":
+    text = "Summation is the sum of all values.";
+    ASE.say("Summation is the sum of all values.");
+    break;
+    //}
+//}
+                        //Sample Mean  {
+    case "what is the equation for sample mean":
+    text = "The equation for sample mean is summation of x divided by n. x are the data values and n is the size of the sample population";
+    window.open("../../../BLACC_Search/Math_Operations/Mean.html", "../../../BLACC_Search/Math_Operations/Mean.html", "width=400,height=400");
+    break;
+//}
+                        //Mode Sample  {
+    case "what is the mode":
+    text = "The mode is the number within a data set that repeats the most";
+    window.open("../../../BLACC_Search/Math_Operations/Mode.html", "../../../BLACC_Search/Math_Operations/Mode.html", "width=400,height=400");
+    break;
+//}
+                        //Median  {
+    case "what is the median":
+    text = "The median is the number that is placed in the middle when all numbers are placed from least to greatest";
+    window.open("../../../BLACC_Search/Math_Operations/Mode.html", "../../../BLACC_Search/Math_Operations/Mode.html", "width=400,height=400");
+    break;
+                    //}
+//}
+                    //Organizing and Summarizing Data  {
+                                
+                                
+                            //}
+                    //Producing Data  {
+                                
+                                
+                            //}
+                    //Probability  {
+                                
+                                
+                            //}
+                    //Statistical Distributions  {
+                                
+                                
+                            //}
+                    //Random Variable  {
+                                
+                                
+                            //}
+                    //Binomial Distributions  {
+                                
+                                
+                            //}
+                    //Geometric Distributions  {
+                                
+                            //}
+                    //Sampling Distributions  {
+                                
+                                
+                            //}
+                    //Statistical Inference Overview  {
+                                
+                                
+                            //}
+                    //Inference With z Distributions  {
+                                
+                                
+                            //}
+                    //Inference with t Distributions  {
+                                
+                                
+                            //}
+                    //Inference for Populations  {
+                                
+                                
+                            //}
+                    //Chi Square Procedures  {
+                                
+                                
+                            //}
+                    //Linear Regression and Correlation  {
+                                
+                                
+                            //}
+//}
+                //Algebra  {
+                    //One Variable Equations and Inequalities  {
+                        
+                        
+                    //}
+                    //Linear Equations, Functions and Graphs  {
+                        
+                        
+                    //}
+                    //Sequences  {
+                        
+                        
+                    //}
+                    //System of Equations  {
+                        
+                        
+                    //}
+                    //Two Variable Inequalities  {
+                        
+                        
+                    //}
+                    //Functions  {
+                    
+  //}
+                    //Absolute Value Equations, Functions and Inequalities  {
+                        
+                        
+                    //}
+                    //Quadratic Equations and Functions  {
+                        
+                        
+                    //}
+                    //Polyomial Expressions, Equations and Functions  {
+                        
+                        
+                    //}
+                    //Exponential and Logarithmic Functions  {
+                        
+                        
+                    //}
+                    //Radical Equations and Functions  {
+                        
+                        
+                    //}
+                    //Rational Expressions, Equations and Functions  {
+                        
+ 
+                    //}
+//}
+                //Algebra 1 {
+                    //Algebra Functions  {
+                      //Introduction To Variables  {
+                                
+                                
+                            //}
+                      //Substitution And Evaluating Expressions  {
+                                
+                                
+                            //}
+                      //Combining Like Terms  {
+                                
+                                
+                            //}
+                      //Introduction To Equivalent Experiences  {
+                                
+                                
+                            //}
+                      //Division By Zero  {
+                                
+                                
+                            //}
+                            //}
+                    //Solving Equations And Inequalities {
+                      //Linear Equations With Variables On Both Sides {
+                          
+                      //}
+                      //Linear Equations With Parentheses {
+                          
+                      //}
+                      //Analyzing The Number Of Solutions To Linear Equations {
+                          
+                      //}
+                      //Lineaar Equations With Unknown Coefficients {
+                          
+                      //}
+                      //Multi-stwp Inequalities {
+                          
+                      //}
+                      //Compound Inequalities {
+                          
+                      //}
+                    //}
+                    //Working With Units {
+                      //Rule Conversion {
+                      //}
+                      //Appropriate Units {
+                          
+                      //}
+                      //Word Problems With Multiple Units {
+                          
+                      //}
+                    //}
+                    //Linear Equations And Graphs {
+                       //Two-variable Linear Equation Introduction {
+                       
+                       //}
+                       //Slope {
+                           
+                       //}
+                       //Horizontal And Vertical Lines  {
+                           
+                       //}
+                       //X-intercept And Y-intercept  {
+                           
+                       //}
+                       //Applying Intercepts And Slope  {
+                           
+                       //}
+                       //
+                    //}
+                //}
+                //Geometry {
+                    //Area of Rectangle  {
+                                                        
+    case "area of a rectangle":
+    text = "area of a rectangle.";
+    window.open("./Research/Math_Operations/Rect.html", "./Research/Math_Operations/Rect.html", "width=400,height=250");
+    break;
+                                                        
+    case "calculate the area of a rectangle":
+    text = "area of a rectangle.";
+    window.open("./Research/Math_Operations/Rect.html", "./Research/Math_Operations/Rect.html", "width=400,height=250");
+    break;
+                                                        
+    case "what is the area of a rectangle":
+    text = "area of a rectangle.";
+    window.open("./Research/Math_Operations/Rect.html", "./Research/Math_Operations/Rect.html", "width=400,height=250");
+    break;
+                                                        
+    case "area of a rectangle missing width ":
+    text = "area of a rectangle.";
+    window.open("./Research/Math_Operations/Rect_Missing_Width.html", "./Research/Math_Operations/Rect_Missing_Width.html", "width=400,height=250");
+    break;
+                                                        
+    case "area of a rectangle missing length":
+    text = "area of a rectangle.";
+    window.open("./Research/Math_Operations/Rect_Missing_Width.html", "./Research/Math_Operations/Rect_Missing_Width.html", "width=400,height=250");
+    break;
+                        
+                    //}
+                    //Area of Triangle  {
+                                                        
+    case "area of a triangle":
+    text = "area of a triangle.";
+    window.open("./Research/Math_Operations/Triangle.html", "./Research/Math_Operations/Triangle.html", "width=400,height=250");
+    break;
+    
+    case "calculate the area of a triangle":
+    text = "area of a triangle.";
+    window.open("./Research/Math_Operations/Triangle.html", "./Research/Math_Operations/Triangle.html", "width=400,height=250");
+    break;
+                                                        
+    case "what is the area of a triangle":
+    text = "area of a triangle.";
+    window.open("./Research/Math_Operations/Triangle.html", "./Research/Math_Operations/Triangle.html", "width=400,height=250");
+    break;
+                                                        
+    case "area of a triangle missing base":
+    text = "area of a triangle.";
+    window.open("./Research/Math_Operations/Triangle_Missing_Base.html", "./Research/Math_Operations/Triangle_Missing_Base.html", "width=400,height=250");
+    break;
+                                                        
+    case "area of a triangle missing height":
+    text = "area of a triangle.";
+    window.open("./Research/Math_Operations/Triangle_Missing_Base.html", "./Research/Math_Operations/Triangle_Missing_Base.html", "width=400,height=250");
+    break;
+                        
+                    //}
+                    //Area of Circle  {
+                                                        
+    case "calculate the area of a circle":
+    text = "area of a circle.";
+    window.open("./Research/Math_Operations/Circle.html", "./Research/Math_Operations/Circle.html", "width=400,height=250");
+    break;
+                                                        
+    case "area of a circle":
+    text = "area of a circle.";
+    window.open("./Research/Math_Operations/Circle.html", "./Research/Math_Operations/Circle.html", "width=400,height=250");
+    break;
+                                                        
+    case "what is the area of a circle":
+    text = "area of a circle.";
+    window.open("./Research/Math_Operations/Circle.html", "./Research/Math_Operations/Circle.html", "width=400,height=250");
+    break;
+                                                        
+    case "area of a circle missing radius":
+    text = "area of a circle.";
+    window.open("./Research/Math_Operations/Circle_Missing_Radius.html", "./Research/Math_Operations/Circle_Missing_Radius.html", "width=400,height=250");
+    break;
+                         
+                    //}
+//}
+                //Trigonometry  {
+                    //Cosine  {
+    case "angle of cosine":
+    text = "angle of in cosine.";
+    window.open("./Angle_Cosine.html", "./Angle_Cosine.html", "width=400,height=400");
+    break;
+    
+    case "what is the angle of cosine":
+    text = "angle of in cosine.";
+    window.open("./Angle_Cosine.html", "./Angle_Cosine.html", "width=400,height=400");
+    break;
+    
+    case "calculate the angle of cosine":
+    text = "angle of in cosine.";
+    window.open("./Angle_Cosine.html", "./Angle_Cosine.html", "width=400,height=400");
+    break;
+    
+    //}
+//}
+//}
+
+                            
+//}
+
+                            //}
+
+                            //Conditional  {
+                            case "bring up Mass alert":
+                                text = "bring up Mass alert";
+                                ASE.say("Okay. Stand by.");
+                                if (emergency < relax) {
+                                    alert("Emergency");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "width=400,height=400");
+                                } else {
+                                    alert("Relax");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "width=400,height=400");
+
+                                };
+                                break;
+
+                            //}
+                            //reponse to not being able to hear me
+
+                            default:
+                                text = "My apologies.";
+                                ASE.say("I could not hear you clearly can you please repeat that");
+                                break;
+                        }
+
+                        document.getElementById("demo").innerHTML = text;
+                    }
+
