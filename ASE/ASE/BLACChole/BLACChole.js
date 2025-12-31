4387be7ef6ddef1d85a60ff966b96bbb667cbcca
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2025-12-31 00:44:03 -0800

    Add files via upload
    Signed-off-by: Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>

--- ASE/ASE/BLACChole/BLACChole.js
+++ ASE/ASE/BLACChole/BLACChole.js
@@ -1,1 +1,694 @@
 
+
+                    function myFunction() {
+
+                        //Variables {
+
+                        var text;
+
+                        var consciousness = document.getElementById("output").value;
+
+                        var ASE = new Artyom();
+
+                        var emergency = 1;
+
+                        var relax = 2;
+
+                        //}
+
+                        switch (consciousness) {
+
+                            
+
+//Digital Modules  {
+
+
+   
+
+
+    //Knowledge Module {
+
+      
+
+  
+
+        //BLACChole  {
+
+                //Main Site  {
+
+    case "BLACChole":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Website_Template_BLACC_Corporation_Computer_Science_Project_BLACChole.html", target="_blank", "../../../BLACChole/Website_Template_BLACC_Corporation_Computer_Science_Project_BLACChole.html", "width=400,height=400");
+
+    break;
+
+    
+
+    //}
+
+                //Asclepieion Of B.L.A.C.C. {
+
+                            
+
+    case "BLACChole - Asclepieion Of BLACC":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Website_Template_BLACC_Corporation_Computer_Science_Project_BLACChole.html", target="_blank", "../../../BLACChole/Website_Template_BLACC_Corporation_Computer_Science_Project_BLACChole.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Asclepieion Of BLACC":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Website_Template_BLACC_Corporation_Computer_Science_Project_BLACChole.html", target="_blank", "../../../BLACChole/Website_Template_BLACC_Corporation_Computer_Science_Project_BLACChole.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Asclepieion Of B.L.A.C.C.":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Website_Template_BLACC_Corporation_Computer_Science_Project_BLACChole.html", target="_blank", "../../../BLACChole/Website_Template_BLACC_Corporation_Computer_Science_Project_BLACChole.html", "width=400,height=400");
+
+    break;
+
+                
+
+            //}
+
+                //Audio  {
+
+                            
+
+    case "BLACChole - Audio":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Audio/BLACChole_Audio_Home.html", target="_blank", "../../../BLACChole/Audio/BLACChole_Audio_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Audio":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Audio/BLACChole_Audio_Home.html", target="_blank", "../../../BLACChole/Audio/BLACChole_Audio_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //B.L.A.C.C. Ark  {
+
+                            
+
+    case "BLACChole - BLACC Ark":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Ark/BLACChole_BLACC_Ark_Home.html", target="_blank", "../../../BLACChole/BLACC_Ark/BLACChole_BLACC_Ark_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "BLACC Ark":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Ark/BLACChole_BLACC_Ark_Home.html", target="_blank", "../../../BLACChole/BLACC_Ark/BLACChole_BLACC_Ark_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "B.L.A.C.C. Ark":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Ark/BLACChole_BLACC_Ark_Home.html", target="_blank", "../../../BLACChole/BLACC_Ark/BLACChole_BLACC_Ark_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //B.L.A.C.C. Connect  {
+
+                            
+
+    case "BLACChole - BLACC Connect":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Connect/BLACChole_BLACC_Connect_Home.html", target="_blank", "../../../BLACChole/BLACC_Connect/BLACChole_BLACC_Connect_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "BLACC Connect":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Connect/BLACChole_BLACC_Connect_Home.html", target="_blank", "../../../BLACChole/BLACC_Connect/BLACChole_BLACC_Connect_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "B.L.A.C.C. Connect":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Connect/BLACChole_BLACC_Connect_Home.html", target="_blank", "../../../BLACChole/BLACC_Connect/BLACChole_BLACC_Connect_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //B.L.A.C.C. Music  {
+
+                            
+
+    case "BLACChole - BLACC Music":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Music/BLACChole_BLACC_Music_Home.html", target="_blank", "../../../BLACChole/BLACC_Music/BLACChole_BLACC_Music_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "BLACC Music":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Music/BLACChole_BLACC_Music_Home.html", target="_blank", "../../../BLACChole/BLACC_Music/BLACChole_BLACC_Music_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "B.L.A.C.C. Music":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Music/BLACChole_BLACC_Music_Home.html", target="_blank", "../../../BLACChole/BLACC_Music/BLACChole_BLACC_Music_Home.html", "width=400,height=400");
+
+    break;
+
+                        
+
+                //}
+
+                //B.L.A.C.C. Telecommunication  {
+
+                            
+
+    case "BLACChole - BLACC Tele":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Tele/BLACChole_BLACC_Tele_Home.html", target="_blank", "../../../BLACChole/BLACC_Tele/BLACChole_BLACC_Tele_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "BLACC Tele":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Tele/BLACChole_BLACC_Tele_Home.html", target="_blank", "../../../BLACChole/BLACC_Tele/BLACChole_BLACC_Tele_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "B.L.A.C.C. Tele":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/BLACC_Tele/BLACChole_BLACC_Tele_Home.html", target="_blank", "../../../BLACChole/BLACC_Tele/BLACChole_BLACC_Tele_Home.html", "width=400,height=400");
+
+    break;
+
+                //}
+
+                //Blacklining  {
+
+                            
+
+    case "BLACChole - Blacklining":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Blacklining/BLACChole_Blacklining_Home.html", target="_blank", "../../../BLACChole/Blacklining/BLACChole_Blacklining_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Blacklining":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Blacklining/BLACChole_Blacklining_Home.html", target="_blank", "../../../BLACChole/Blacklining/BLACChole_Blacklining_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //Categories  {
+
+                            
+
+    case "BLACChole - Categories":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Categories/BLACChole_Categories_Home.html", target="_blank", "../../../BLACChole/Categories/BLACChole_Categories_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Categories":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Categories/BLACChole_Categories_Home.html", target="_blank", "../../../BLACChole/Categories/BLACChole_Categories_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //Eye  {
+
+                            
+
+    case "BLACChole - Eye":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Eye/BLACChole_Eye_Home.html", target="_blank", "../../../BLACChole/Eye/BLACChole_Eye_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Eye":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Eye/BLACChole_Eye_Home.html", target="_blank", "../../../BLACChole/Eye/BLACChole_Eye_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //Movies  {
+
+                            
+
+    case "BLACChole - Movies":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Movies/BLACChole_Movies_Home.html", target="_blank", "../../../BLACChole/Movies/BLACChole_Movies_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Movies":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Movies/BLACChole_Movies_Home.html", target="_blank", "../../../BLACChole/Movies/BLACChole_Movies_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //Music  {
+
+                            
+
+    case "BLACChole - Music":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Music/BLACChole_Music_Home.html", target="_blank", "../../../BLACChole/Music/BLACChole_Music_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Music":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Music/BLACChole_Music_Home.html", target="_blank", "../../../BLACChole/Music/BLACChole_Music_Home.html", "width=400,height=400");
+
+    break;
+
+                //}
+
+                //New Nexxus  {
+
+                            
+
+    case "BLACChole - New Nexxus":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/New_Nexxus/BLACChole_New_Nexxus_Home.html", target="_blank", "../../../BLACChole/New_Nexxus/BLACChole_New_Nexxus_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "New Nexxus":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/New_Nexxus/BLACChole_New_Nexxus_Home.html", target="_blank", "../../../BLACChole/New_Nexxus/BLACChole_New_Nexxus_Home.html", "width=400,height=400");
+
+    break;
+
+                //}
+
+                //NFT  {
+
+                            
+
+    case "BLACChole - NFT":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/NFT/BLACChole_NFT_Home.html", target="_blank", "../../../BLACChole/NFT/BLACChole_NFT_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "NFT":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/NFT/BLACChole_NFT_Home.html", target="_blank", "../../../BLACChole/NFT/BLACChole_NFT_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //OneiroNet  {
+
+                            
+
+    case "BLACChole - OneiroNet":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/OneiroNet/BLACChole_OneiroNet_Home.html", target="_blank", "../../../BLACChole/NFT/BLACChole_NFT_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "OneiroNet":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/OneiroNet/BLACChole_OneiroNet_Home.html", target="_blank", "../../../BLACChole/NFT/BLACChole_NFT_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //Pharaoh History  {
+
+                            
+
+    case "BLACChole - Pharaoh History":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Pharaoh_History/BLACChole_Pharaoh_History_Home.html", target="_blank", "../../../BLACChole/Pharaoh_History/BLACChole_Pharaoh_History_Home.html", "width=400,height=400");
+
+    break;
+
+
+                            
+
+    case "Pharaoh History":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Pharaoh_History/BLACChole_Pharaoh_History_Home.html", target="_blank", "../../../BLACChole/Pharaoh_History/BLACChole_Pharaoh_History_Home.html", "width=400,height=400");
+
+    break;                    
+
+                //}
+
+                //Upload  {
+
+                            
+
+    case "BLACChole - Upload":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Upload/BLACChole_Upload_Home.html", target="_blank", "../../../BLACChole/Upload/BLACChole_Upload_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Upload":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Upload/BLACChole_Upload_Home.html", target="_blank", "../../../BLACChole/Upload/BLACChole_Upload_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+                //Videos  {
+
+                            
+
+    case "BLACChole - Videos":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Videos/BLACChole_Videos_Home.html", target="_blank", "../../../BLACChole/Videos/BLACChole_Videos_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Videos":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Videos/BLACChole_Videos_Home.html", target="_blank", "../../../BLACChole/Videos/BLACChole_Videos_Home.html", "width=400,height=400");
+
+    break;
+
+                //}
+
+                //Vital Mathematics  {
+
+                    //Direct Link  {
+
+    case "BLACChole - Vital Mathematics":
+
+    text = "Stand By.";
+
+    window.open("../../../BLACChole/Vital_Mathematics/BLACChole_Vital_Mathematics_Home.html", target="_blank", "../../../BLACChole/Vital_Mathematics/BLACChole_Vital_Mathematics_Home.html", "width=400,height=400");
+
+    break;
+
+    
+
+    case "Vital Mathematics":
+
+    text = "Stand By.";
+
+    window.open("../../../BLACChole/Vital_Mathematics/BLACChole_Vital_Mathematics_Home.html", target="_blank", "../../../BLACChole/Vital_Mathematics/BLACChole_Vital_Mathematics_Home.html", "width=400,height=400");
+
+    break;
+
+                    //}
+
+                    //Statistics - Mode  {
+
+    case "Statistics - Mode Video":
+
+    text = "Stand By.";
+
+    window.open("../../../BLACChole/Vital_Mathematics/Vital_Mathematics_Mode.html", target="_blank", "../../../BLACChole/Vital_Mathematics/Vital_Mathematics_Mode.html", "width=400,height=400");
+
+    break;
+
+    
+
+    case "Statistics - Mode":
+
+    text = "Stand By.";
+
+    window.open("../../../BLACChole/Vital_Mathematics/Vital_Mathematics_Mode.html", target="_blank", "../../../BLACChole/Vital_Mathematics/Vital_Mathematics_Mode.html", "width=400,height=400");
+
+    break;
+
+                        
+
+                    //}
+
+                    //Statistics - Concepts Regarding The Mode  {
+
+    case "Statistics - Concepts Regarding The Mode Video":
+
+    text = "Stand By.";
+
+    window.open("../../../BLACChole/Vital_Mathematics/Vital_Mathematics_Concepts_Concerning_The_Mode.html", target="_blank", "../../../BLACChole/Vital_Mathematics/Vital_Mathematics_Concepts_Concerning_The_Mode.html", "width=400,height=400");
+
+    break;
+
+    
+
+    case "Statistics - Concepts Regarding The Mode":
+
+    text = "Stand By.";
+
+    window.open("../../../BLACChole/Vital_Mathematics/Vital_Mathematics_Concepts_Concerning_The_Mode.html", target="_blank", "../../../BLACChole/Vital_Mathematics/Vital_Mathematics_Concepts_Concerning_The_Mode.html", "width=400,height=400");
+
+    break;
+
+                        
+
+                    //}
+
+                //}
+
+                //Website Inquiry  {
+
+                            
+
+    case "BLACChole - Website Inquiry":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Website_Inquiry/BLACChole_Website_Inquiry_Home.html", target="_blank", "../../../BLACChole/Website_Inquiry/BLACChole_Website_Inquiry_Home.html", "width=400,height=400");
+
+    break;
+
+                            
+
+    case "Website Inquiry":
+
+    text = "This information has not been proceed yet.";
+
+    window.open("../../../BLACChole/Website_Inquiry/BLACChole_Website_Inquiry_Home.html", target="_blank", "../../../BLACChole/Website_Inquiry/BLACChole_Website_Inquiry_Home.html", "width=400,height=400");
+
+    break;
+
+                    
+
+                //}
+
+            //}
+
+            
+
+                            
+
+//}
+
+
+                            //}
+
+
+                            //Conditional  {
+
+                            case "bring up Mass alert":
+
+                                text = "bring up Mass alert";
+
+                                ASE.say("Okay. Stand by.");
+
+                                if (emergency < relax) {
+
+                                    alert("Emergency");
+
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "width=400,height=400");
+
+                                } else {
+
+                                    alert("Relax");
+
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "width=400,height=400");
+
+
+                                };
+
+                                break;
+
+
+                            //}
+
+                            //reponse to not being able to hear me
+
+
+                            default:
+
+                                text = "My apologies.";
+
+                                ASE.say("I could not hear you clearly, can you please repeat that");
+
+                                break;
+
+                        }
+
+
+                        document.getElementById("demo").innerHTML = text;
+
+                    }
+
+
+
+
