c127ab882998c02920f677a897a0b8f4d56a81a6
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2025-12-31 01:00:42 -0800

    Add files via upload
    Signed-off-by: Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>

--- ASE/ASE/Missions/Missions.js
+++ ASE/ASE/Missions/Missions.js
@@ -1,1 +1,248 @@
 
+                    function myFunction() {
+                        //Variables {
+                        var text;
+                        var consciousness = document.getElementById("output").value;
+                        var ASE = new Artyom();
+                        var emergency = 1;
+                        var relax = 2;
+                        //}
+                        switch (consciousness) {
+                            
+//Digital Modules  {
+
+   
+
+    //Knowledge Module {
+      
+                            
+        //Missions {
+
+            //Missions Lv.0   {
+            
+                case "Level zero missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_0.html", "./Missions/Mission_List_LV_0.html", "width=400,height=400");
+                break;
+            
+                case "Level 0 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_0.html", "./Missions/Mission_List_LV_0.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.1 {
+            
+                case "Level one missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_1.html", "./Missions/Mission_List_LV_1.html", "width=400,height=400");
+                break;
+            
+                case "Level 1 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_1.html", "./Missions/Mission_List_LV_1.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.2  {
+            
+                case "Level two missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_2.html", "./Missions/Mission_List_LV_2.html", "width=400,height=400");
+                break;
+            
+                case "Level 2 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_2.html", "./Missions/Mission_List_LV_2.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.3  {
+            
+                case "Level three missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_3.html", "./Missions/Mission_List_LV_3.html", "width=400,height=400");
+                break;
+            
+                case "Level 3 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_3.html", "./Missions/Mission_List_LV_3.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.4  {
+            
+                case "Level four missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_4.html", "./Missions/Mission_List_LV_4.html", "width=400,height=400");
+                break;
+            
+                case "Level 4 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_4.html", "./Missions/Mission_List_LV_4.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.5  {
+            
+                case "Level five missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_5.html", "./Missions/Mission_List_LV_5.html", "width=400,height=400");
+                break;
+            
+                case "Level 5 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_5.html", "./Missions/Mission_List_LV_5.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.6  {
+            
+                case "Level six missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_6.html", "./Missions/Mission_List_LV_6.html", "width=400,height=400");
+                break;
+            
+                case "Level 6 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_6.html", "./Missions/Mission_List_LV_6.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.7  {
+            
+                case "Level seven missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_7.html", "./Missions/Mission_List_LV_7.html", "width=400,height=400");
+                break;
+            
+                case "Level 7 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_7.html", "./Missions/Mission_List_LV_7.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.8  {
+            
+                case "Level eight missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_8.html", "./Missions/Mission_List_LV_8.html", "width=400,height=400");
+                break;
+            
+                case "Level 8 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_8.html", "./Missions/Mission_List_LV_8.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.9  {
+            
+                case "Level nine missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_9.html", "./Missions/Mission_List_LV_9.html", "width=400,height=400");
+                break;
+            
+                case "Level 9 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_9.html", "./Missions/Mission_List_LV_9.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.10  {
+            
+                case "Level ten missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_10.html", "./Missions/Mission_List_LV_10.html", "width=400,height=400");
+                break;
+            
+                case "Level 10 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_10.html", "./Missions/Mission_List_LV_10.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.11  {
+            
+                case "Level eleven missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_11.html", "./Missions/Mission_List_LV_11.html", "width=400,height=400");
+                break;
+            
+                case "Level 11 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_11.html", "./Missions/Mission_List_LV_11.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.12  {
+            
+                case "Level twelve missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_12.html", "./Missions/Mission_List_LV_12.html", "width=400,height=400");
+                break;
+            
+                case "Level 12 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_12.html", "./Missions/Mission_List_LV_12.html", "width=400,height=400");
+                break;
+            //}
+            //Missions Lv.13 {
+            
+                case "Level thirteen missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_13.html", "./Missions/Mission_List_LV_13.html", "width=400,height=400");
+                break;
+            
+                case "Level 13 missions":
+                text = "Processing...";
+                ASE.say("Stand By.");
+                window.open("./Missions/Mission_List_LV_13.html", "./Missions/Mission_List_LV_13.html", "width=400,height=400");
+                break;
+            //}
+                            //}
+                            
+//}
+
+                            //}
+
+                            //Conditional  {
+                            case "bring up Mass alert":
+                                text = "bring up Mass alert";
+                                ASE.say("Okay. Stand by.");
+                                if (emergency < relax) {
+                                    alert("Emergency");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "width=400,height=400");
+                                } else {
+                                    alert("Relax");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "width=400,height=400");
+
+                                };
+                                break;
+
+                            //}
+                            //reponse to not being able to hear me
+
+                            default:
+                                text = "My apologies.";
+                                ASE.say("I could not hear you clearly can you please repeat that");
+                                break;
+                        }
+
+                        document.getElementById("demo").innerHTML = text;
+                    }
+
