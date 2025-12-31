28676774027f83d099c86494b45aee5f30c4d98f
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2025-12-31 00:40:00 -0800

    Add files via upload
    Signed-off-by: Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>

---
+++ ASE/ASE/Arthenya/import.js
@@ -0,0 +1,23 @@
+//module1.js 
+let Arthenya =
+{
+	name1: "Aardvark",
+	name2: "Abyssinian",
+	name3: "Acadian Flycatcher",
+	name4: "Ackie Monitor",
+	name5: "Adélie Penguin",
+	name6: "Aesculapian Snake",
+	name7: "Afghan Hound",
+	name8: "African Bush Elephant",
+	name9: "African Clawed Frog",
+	name10: "African Forest Elephant",
+	name11: "African Grey Parrot",
+	name12: "African Palm Civet",
+	name13: "African Sugarcane Borer",
+	name14: "Aardwolf",
+	name15: "Abyssinian Guinea Pig",
+	name16: "Achrioptera Manga",
+	name17: "Addax",
+	name18: "Admiral Butterfly",
+	name19: "Affenpinscher",
+};
