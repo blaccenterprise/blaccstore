37ebba62723b4dcbb3913a44e4c9cb2aa7abad17
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2025-12-31 01:08:55 -0800

    Add files via upload
    Signed-off-by: Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>

--- ASE/ASE/Music_Safe/Music_Safe.js
+++ ASE/ASE/Music_Safe/Music_Safe.js
@@ -1,1 +1,52 @@
 
+                    function myFunction() {
+                        //Variables {
+                        var text;
+                        var consciousness = document.getElementById("output").value;
+                        var ASE = new Artyom();
+                        var emergency = 1;
+                        var relax = 2;
+                        //}
+                        switch (consciousness) {
+                            
+//Digital Modules  {
+
+    //Knowledge Module {
+            
+        //Music Safe  {
+    case "Music Safe":
+    text = "This information has not been proceed yet.";
+    window.open("./Media/Music_Safe/Music_Safe.html", target="_self", "./Media/Music_Safe/Music_Safe.html", "width=400,height=400");
+    break;
+            //}
+                            
+//}
+
+                            //}
+
+                            //Conditional  {
+                            case "bring up Mass alert":
+                                text = "bring up Mass alert";
+                                ASE.say("Okay. Stand by.");
+                                if (emergency < relax) {
+                                    alert("Emergency");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "width=400,height=400");
+                                } else {
+                                    alert("Relax");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "width=400,height=400");
+
+                                };
+                                break;
+
+                            //}
+                            //reponse to not being able to hear me
+
+                            default:
+                                text = "My apologies.";
+                                ASE.say("I could not hear you clearly King Carpenter can you please repeat that");
+                                break;
+                        }
+
+                        document.getElementById("demo").innerHTML = text;
+                    }
+
